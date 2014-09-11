a496177fd966961dd1fc6402f8531839bfe45ba5
Daniel Lacroix <dlacroix@erasme.org>
2014-09-11 14:37:50 +0200

    add the "symbolic link" resource type

---
+++ share/www/admin/kjing/link.js
@@ -0,0 +1,10 @@
+﻿
+KJing.Resource.extend('KJing.Link', {
+	linkedResource: undefined,
+
+	getLinkedResource: function() {
+		if(this.linkedResource === undefined)
+			this.linkedResource = KJing.Resource.create(this.getData().link);
+		return this.linkedResource;
+	}
+});
---
+++ share/www/admin/kjing/linkview.js
@@ -0,0 +1,22 @@
+﻿
+KJing.View.extend('KJing.LinkView', {
+	resource: undefined,
+
+	constructor: function(config) {
+		this.resource = config.resource;
+		delete(config.resource);
+
+		if(this.resource.getIsReady())
+			this.onResourceReady();
+		else
+			this.connect(this.resource, 'ready', this.onResourceReady);
+
+		this.setContent(new Ui.Rectangle({ fill: 'purple', margin: 10 }));
+	},
+
+	onResourceReady: function() {
+		console.log('resource ready');
+		var linkedResource = this.resource.getLinkedResource();
+		this.setContent(KJing.View.create(this.view, linkedResource));
+	}
+});
