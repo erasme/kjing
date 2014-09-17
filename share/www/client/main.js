0f4271c5a6f24612c079f2d11a18be65988a12fe
Daniel Lacroix <dlacroix@erasme.org>
2014-09-17 17:13:30 +0200

    start using "play list" on client

--- share/www/client/main.js
+++ share/www/client/main.js
@@ -3,22 +3,31 @@ Ui.App.extend('KJing.ClientApp', {
 	sessionId: undefined,
 	deviceId: undefined,
 	device: undefined,
+	mainBox: undefined,
 	transBox: undefined,
 	path: undefined,
 	viewer: undefined,
 	clientData: undefined,
 	state: undefined,
 	capabilities: undefined,
-	
+	networkIcon: undefined,
+	resource: undefined,
+	playList: undefined,
+
 	constructor: function(config) {
+		this.mainBox = new Ui.LBox();
+		this.setContent(this.mainBox);
+
 		this.transBox = new Ui.TransitionBox();
-		this.setContent(this.transBox);
+		this.mainBox.setContent(this.transBox);
+
+		this.playList = [];
 
 		this.capabilities = {
 			sound: true,
 			width: this.getLayoutWidth(),
 			height: this.getLayoutHeight()
-		}
+		};
 		this.state = {};
 
 		var loginJson = {};
@@ -58,6 +67,10 @@ Ui.App.extend('KJing.ClientApp', {
 	getDevice: function() {
 		return this.device;
 	},
+
+	getUser: function() {
+		return this.getDevice();
+	},
 	
 	setPath: function(path) {
 		this.path = path;
@@ -65,12 +78,25 @@ Ui.App.extend('KJing.ClientApp', {
 			this.transBox.replaceContent(new Ui.Element());
 		}
 		else {
+			console.log('setPath('+path+')');
+
+			if((this.resource !== undefined) && !this.resource.getIsReady())
+				this.disconnect(this.resource, 'ready', this.onResourceReady);
+
+			this.resource = KJing.File.create(path, 4);
+			if(this.resource.getIsReady())
+				this.onResourceReady();
+			else
+				this.connect(this.resource, 'ready', this.onResourceReady);
+
 			var pos = path.lastIndexOf(':');
 			var share = path.substring(5,pos);
 			var file = path.substring(pos+1);
-			var request = new Core.HttpRequest({ method: 'GET', url: '/cloud/storage/'+share+'/'+file });
+			var request = new Core.HttpRequest({ method: 'GET', url: '/cloud/storage/'+share+'/'+file+'?depth=4' });
 			this.connect(request, 'done', function() {
-				var json = request.getResponseJSON();			
+				var json = request.getResponseJSON();
+				console.log(json);
+
 				this.viewer = new Storage.FileViewer({ storage: share, file: json, controller: this });
 				this.connect(this.viewer, 'end', this.onViewerPlayEnd);
 				this.viewer.play();
@@ -78,6 +104,36 @@ Ui.App.extend('KJing.ClientApp', {
 			});
 			request.send();
 		}
+	},
+
+	onResourceReady: function() {
+		console.log('onResourceReady');
+		console.log(this.resource);
+
+		this.playList = [];
+		this.generatePlayList(this.resource);
+		console.log(this.playList);
+	},
+
+	generatePlayList: function(resource) {
+		if((resource !== undefined) && (resource.getIsReady())) {
+			if(resource.getMimetype() === 'application/x-directory') {
+				if(resource.getIsChildrenReady()) {
+					for(var i = 0; i < resource.getChildren().length; i++)
+						this.generatePlayList(resource.getChildren()[i]);
+				}
+			}
+			else {
+				this.playList.push({ id: this.playList.length, file: resource, transform: { x: 0, y: 0, scale: 1 } });
+			}
+		}
+		var list = [];
+		for(var i = 0; i < this.playList.length; i++) {
+			var item = this.playList[i];
+			list.push({ id: item.id, transform: item.transform, path: item.file.getId() });
+		}
+		this.state.list = list;
+		this.device.notifyClientData();
 	},
 	
 	setPosition: function(position) {
@@ -92,16 +148,35 @@ Ui.App.extend('KJing.ClientApp', {
 	
 	onLoginDone: function() {
 		this.device = KJing.Device.create(this.deviceId);
+		this.device.monitor();
+		this.connect(this.device, 'monitor', this.onDeviceMonitor);
+		this.connect(this.device, 'unmonitor', this.onDeviceUnMonitor);
 
 		this.clientData = this.device.getClientData();
 		this.clientData.capabilities = this.capabilities;
 		this.clientData.state = this.state;
+		console.log(this.clientData);
+
 		this.device.notifyClientData();
 
 		this.connect(this.device, 'change', this.onDeviceChange);
 		this.connect(this.device, 'clientmessage', this.onDeviceClientMessage);
 		if(this.device.getIsReady())
 			this.onDeviceChange();
+	},
+
+	onDeviceMonitor: function() {
+		if(this.networkIcon !== undefined)
+			this.mainBox.remove(this.networkIcon);
+		this.networkIcon = undefined;
+	},
+
+	onDeviceUnMonitor: function() {
+		this.networkIcon = new Ui.Icon({
+			icon: 'nonetwork', fill: 'red', width: 32, height: 32, opacity: 0.5,
+			verticalAlign: 'top', horizontalAlign: 'right', margin: 10
+		});
+		this.mainBox.append(this.networkIcon);
 	},
 
 	onDeviceChange: function() {
@@ -135,13 +210,18 @@ Ui.App.extend('KJing.ClientApp', {
 			this.setContentTransform(this.state.transform);
 			notifyNeeded = true;
 		}
+		if('volume' in clientMessage) {
+			this.state.volume = clientMessage.volume;
+			//this.setContentTransform(this.state.transform);
+			notifyNeeded = true;
+		}
 		if(notifyNeeded)
 			this.device.notifyClientData();
 	},
 
-	onAppResize: function() {
-		this.capabilities.width = this.getLayoutWidth();
-		this.capabilities.height = this.getLayoutHeight();
+	onAppResize: function(app, width, height) {
+		this.capabilities.width = width;
+		this.capabilities.height = height;
 		if(this.device !== undefined)
 			this.device.notifyClientData();
 	},
